7623f718aa070b9a0338e00aba7e62e0be26d08b
EC2 Default User <ec2-user@ip-172-31-31-155.ap-northeast-1.compute.internal>
2024-05-17 21:23:35 +0000

    [Start] 管理者ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
+ scope module: :public do
   root to: "homes#top"
   devise_for :users
-  devise_for :admin, skip: [:registrations, :password], controllers: {
-    sessions: 'admin/sessions'
-  }
-
-  namespace :admin do
-    get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
-  end
-
-
+  get "homes/about", to: "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit] do
     collection do
       get :search
@@ -20,6 +12,18 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
+ end
+  
+  
+ devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
 
-  get "homes/about", to: "homes#about", as: "about"
+ namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+  
+  
 end
