d4e3d8a743b2b3ec0693678002e790ccfac3fd11
EC2 Default User <ec2-user@ip-172-31-31-155.ap-northeast-1.compute.internal>
2024-05-18 10:50:09 +0000

    Revert "[Start] 管理者ページ"
    This reverts commit 7623f718aa070b9a0338e00aba7e62e0be26d08b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
- scope module: :public do
   root to: "homes#top"
   devise_for :users
-  get "homes/about", to: "homes#about", as: "about"
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit] do
     collection do
       get :search
@@ -12,18 +20,6 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
- end
-  
-  
- devise_for :admin, skip: [:registrations, :password], controllers: {
-    sessions: 'admin/sessions'
-  }
 
- namespace :admin do
-    get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
-  end
-
-  
-  
+  get "homes/about", to: "homes#about", as: "about"
 end
