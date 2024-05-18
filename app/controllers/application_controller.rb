e5db7f1c4cd8474ee65865cd7e7a4981651450f4
EC2 Default User <ec2-user@ip-172-31-31-155.ap-northeast-1.compute.internal>
2024-05-18 11:29:40 +0000

    カリキュラムの管理者としての機能追加を参考に実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  #before_action :authenticate_user!, except: [:top], unless: :admin_controller? # 追加
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -10,6 +11,11 @@ class ApplicationController < ActionController::Base
      about_path
   end
 
+  private
+ 
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
+  end
 
   protected
 
